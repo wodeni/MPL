579d77d76e0dc9babad45cf3fa6ea8a8c2c02ee0
wodeni <wn2155@columbia.edu>
2017-03-27 15:45:54 -0400

    added build script3

--- travis-ci.sh
+++ travis-ci.sh
@@ -1,21 +1,21 @@
 # OPAM version to install
 export OPAM_VERSION=1.2.2
 # OPAM packages needed to build tests
-export OPAM_PACKAGES='ocamlocamlfind ounit llvm.3.6'
+export OPAM_PACKAGES='ocamlfind llvm.3.6'
 
 # install ocaml from apt
 sudo apt-get update -qq
 sudo apt-get install -y ocaml m4 llvm opam
 
 # install opam
-curl -L https://github.com/OCamlPro/opam/archive/${OPAM_VERSION}.tar.gz | tar xz -C /tmp
-pushd /tmp/opam-${OPAM_VERSION}
-./configure
-make
-sudo make install
+# curl -L https://github.com/OCamlPro/opam/archive/${OPAM_VERSION}.tar.gz | tar xz -C /tmp
+# pushd /tmp/opam-${OPAM_VERSION}
+# ./configure
+# make
+# sudo make install
 opam init
 eval `opam config -env`
-popd
+# popd
 
 # install packages from opam
 opam install -q -y ${OPAM_PACKAGES}
