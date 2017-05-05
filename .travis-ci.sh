b1125fb3875f1167a5286155ed04468ace155ce3
wodeni <wn2155@columbia.edu>
2017-05-05 12:43:29 -0400

    added install routine from Edwards

--- .travis-ci.sh
+++ .travis-ci.sh
@@ -1,24 +1,12 @@
 # OPAM version to install
 export OPAM_VERSION=1.2.2
-# OPAM packages needed to build tests
-export OPAM_PACKAGES='ocamlfind ounit'
 
 # install ocaml from apt
 sudo apt-get update -qq
-sudo apt-get install -qq ocaml
-
-# install opam
-curl -L https://github.com/OCamlPro/opam/archive/${OPAM_VERSION}.tar.gz | tar xz -C /tmp
-pushd /tmp/opam-${OPAM_VERSION}
-./configure
-make
-sudo make install
+sudo apt-get install -y ocaml m4 llvm opam
 opam init
-eval `opam config -env`
-popd
-
-# install packages from opam
-opam install -q -y ${OPAM_PACKAGES}
+opam install llvm.3.6 ocamlfind
+eval `opam config env`
 
 # compile & run tests (here assuming OASIS DevFiles)
 cd src
