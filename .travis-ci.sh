7a5d7fa3900569c0cb291052565573cb05e8ea95
wodeni <wn2155@columbia.edu>
2017-05-05 14:00:46 -0400

    auto commit

--- .travis-ci.sh
+++ .travis-ci.sh
@@ -6,9 +6,9 @@ sudo add-apt-repository ppa:avsm/ocaml42+opam120 -y
 sudo apt-get update
 sudo apt-get install -y ocaml m4 llvm opam
 opam init -y
-opam install llvm.3.4 ocamlfind -y
+opam install  ocamlfind -y
+# opam install  llvm.3.4 -y
 eval `opam config env`
-
 
 # compile & run tests (here assuming OASIS DevFiles)
 cd src
